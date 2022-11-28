f105cd90f7e20b783ed58f4b91e5a7a4ef382480
Alexandru Frunza <frunza.alexandru.2004@gmail.com>
2022-11-28 22:23:22 +0200

    Add functions to initialize data types

--- piecetable.h
+++ piecetable.h
@@ -3,25 +3,54 @@
 #ifndef NOTEIM_PIECETABLE_H_
 #define NOTEIM_PIECETABLE_H_
 
-struct BufferElement
+#define MAX_LENGTH_BUFFER 2047
+
+struct TextAttribute
 {
-    char value;
     Color bg;
-    Color font;
+    Color fontColor;
+
+    TextAttribute *prev;
+    TextAttribute *next;
 };
+
+// TextAttributeList* initTextAttribute();
+bool textAttributeIsEqual(TextAttribute *ta1, TextAttribute *ta2);
+
+struct TextAttributesList
+{
+    unsigned int length;
+    TextAttribute *first;
+    TextAttribute *last;
+};
+
+// TextAtrributesList* initTextAttributesList();
 
 struct Buffer
 {
-    BufferElement value[1000];
+    TextAttributesList *textAttributesList;
+    char text[MAX_LENGTH_BUFFER+1];
+    // Maximum length will be 2047, last element is reserved for '\0'
+    unsigned int length:10;
     Buffer *prev;
     Buffer *next;
 };
 
+Buffer* initBuffer();
+bool bufferIsFull(Buffer *b);
+bool addElementToBuffer(char e, TextAttribute *ta, Buffer *b);
+
 struct BuffersList
 {
     unsigned int length;
-    Buffer *head;
+    Buffer *first;
+    Buffer *last;
 };
+
+BuffersList* initBuffersList();
+void addBuffer(BuffersList *bl, Buffer *b);
+// void removeLastBuffer(BuffersList *bl);
+
 
 struct PieceTableNode
 {
@@ -29,18 +58,32 @@ struct PieceTableNode
     unsigned int start;
     unsigned int length;
     unsigned int numberNewLines;
+
+    PieceTableNode *prev;
+    PieceTableNode *next;
 };
+
+PieceTableNode* initPieceTableNode(unsigned int bufferIndex, unsigned int start, unsigned int length, unsigned int numberNewLines);
+// PieceTableNode* modifyNode();
 
 struct PieceTableNodesList
 {
     unsigned int length;
-    PieceTableNode *head;
+    PieceTableNode *first;
+    PieceTableNode *last;
 };
+
+PieceTableNodesList* initPieceTableNodesList();
+// void addPieceTableNode(PieceTableNodesList *nl, PieceTableNode *n);
+// void removeLastPieceTableNode(PieceTableNodeList *nl);
+
 
 struct PieceTable
 {
     BuffersList *buffersList;
     PieceTableNodesList *nodesList;
 };
+
+PieceTable* initPieceTable();
 
 #endif // NOTEIM_PIECETABLE_H_
